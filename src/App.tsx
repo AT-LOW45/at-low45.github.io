a29f92cc886c9f97faecde7330fa6f7f49b5d253
AT-LOW45 <86214642+AT-LOW45@users.noreply.github.com>
2022-06-04 12:36:54 +0800

    testing config

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Layout from "./components/Layout";
 import Homepage from "./pages/Homepage";
 
 function App() {
+	console.log(process.env.REACT_APP_API);
+	
 	return (
 		<Routes>
 			<Route path='/' element={<Layout />}>
